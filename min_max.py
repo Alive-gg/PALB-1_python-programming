02767c23c4ffea6b7bff95130b23465c9ad873a6
Kishan Tiwari <167543019+Alive-gg@users.noreply.github.com>
2026-01-30 12:34:57 +0530

    Update min_max.py

--- min_max.py
+++ min_max.py
@@ -1,12 +1,10 @@
-arr = [1,2,4,7,5,9]
-
-min = arr[0]
-max = arr[0]
-
-for i in range(1, len(arr)):
-    if arr[i] > min:
-        min = arr[i]
-    if arr[i] < max:
-        max = arr[i]
-print("Minimum = ", min)
-print("Maximum = ", max)
+class Solution:
+    def getMinMax(self, arr):
+        arr_min=arr[0]
+        arr_max=arr[0]
+        for i in range(1,len(arr)):
+            if(arr_min>arr[i]):
+                arr_min=arr[i]
+            if (arr_max<arr[i]):
+                arr_max=arr[i]
+        return(arr_min,arr_max)
